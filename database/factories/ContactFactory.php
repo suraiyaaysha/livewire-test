97c56656f5fa8aa1bc38eee8fe0b70abc5719711
suraiyaaysha <suraiyaaysha.cse@gmail.com>
2024-04-28 23:30:12 +0600

    feat: Contact seeder and factory created.

---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Contact>
+ */
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' =>$this->faker->name,
+            'email' =>$this->faker->unique()->safeEmail(),
+            'phone_number' =>$this->faker->unique()->phoneNumber(),
+            'gender' =>$this->faker->randomElement(['male', 'female', 'other']),
+            'address' =>$this->faker->address(),
+        ];
+    }
+}

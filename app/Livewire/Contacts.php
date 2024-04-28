6588412cd3aad6013de4f88ab3ca48663e799ea9
suraiyaaysha <suraiyaaysha.cse@gmail.com>
2024-04-29 03:52:59 +0600

    feat: search implement in contacts table

--- app/Livewire/Contacts.php
+++ app/Livewire/Contacts.php
@@ -4,15 +4,42 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Contact;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
+use Livewire\WithPagination;
 
 class Contacts extends Component
 {
+    use LivewireAlert;
+    use WithPagination;
+
+    public $q;
+    public $pagination=5;
+
+    // public $contacts;
 
     public function render()
     {
         // fetch contacts
-        $contacts = Contact::latest()->paginate(5);
+        // $contacts = Contact::latest()->paginate(5);
+        // $contacts = Contact::latest()->simplePaginate(5);
 
-        return view('livewire.contacts', compact('contacts'));
+        if(!$this->q) {
+            $contacts = Contact::simplePaginate($this->pagination);
+        }else {
+            $contacts = Contact::where('name', 'like', '%'.$this->q.'%')
+                                ->orWhere('email', 'like', '%'.$this->q.'%')
+                                ->orWhere('gender', 'like', '%'.$this->q.'%')
+                                ->simplePaginate($this->pagination);
+        }
+
+        // return view('livewire.contacts', compact('contacts'));
+        return view('livewire.contacts', [
+            'contacts' =>$contacts,
+        ]);
+    }
+
+    public function updatedQ()
+    {
+        $this->resetPage();
     }
 }
--- resources/views/livewire/contacts.blade.php
+++ resources/views/livewire/contacts.blade.php
@@ -9,8 +9,47 @@
                 <button type="button"
                     class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Download</button>
             </div>
+
+            <!-- Search Input -->
+            {{-- <input type="text" wire:model="search" placeholder="Search Contacts..." class="border border-gray-300 shadow p-3 rounded-lg mb-4"> --}}
+            {{-- <input type="text" wire:model="search" placeholder="Search Contacts..." ... /> --}}
+
+            <!-- Export Buttons -->
+            <div class="mb-4">
+                <button wire:click="exportPDF" class="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg mr-2">Export as PDF</button>
+                <button wire:click="exportCSV" class="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg">Export as CSV</button>
+            </div>
+
         </div>
-        <div class="-mx-4 mt-10 ring-1 ring-gray-300 sm:mx-0 sm:rounded-lg">
+
+        {{-- Search and filter buttons start --}}
+        <div class="my-4 flex sm:flex-row flex-col">
+            <div class="flex flex-row mb-1 sm:mb-0">
+                <div class="relative">
+                    <select wire:mode.live="pagination"
+                        class="h-full rounded-l border block w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                        <option value="5">5</option>
+                        <option value="10">10</option>
+                        <option value="15">15</option>
+                    </select>
+                </div>
+            </div>
+            <div class="block relative">
+                <span class="h-full absolute inset-y-0 left-0 flex items-center pl-2">
+                    <svg viewBox="0 0 24 24" class="h-4 w-4 fill-current text-gray-500">
+                        <path
+                            d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z">
+                        </path>
+                    </svg>
+                </span>
+                <input type="text" wire:model.live="q" placeholder="Search"
+                    class="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
+            </div>
+        </div>
+        {{-- Search and filter buttons end --}}
+
+        <div class="-mx-4 mt-6 ring-1 ring-gray-300 sm:mx-0 sm:rounded-lg">
+
             <table class="min-w-full leading-normal">
                 <thead>
                     <tr>
@@ -42,7 +81,7 @@
                 </thead>
                 <tbody>
                     @foreach ($contacts as $contact)
-                    <tr wire:key=" {{$contact->id}} ">
+                    <tr wire:key="{{$contact->id}}">
                         <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                             {{ $contact->name }}
                         </td>
